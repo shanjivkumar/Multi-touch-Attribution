a8f3480fb4b47366125f71c0b39f2c96563f292e
shanjivkumar <shanjivkumar@gmail.com>
2017-07-18 21:36:01 +0530

    Revert

--- ui.R
+++ ui.R
@@ -3,15 +3,15 @@ library(shinydashboard)
 
 
 
-dashboardPage(
+ui <- dashboardPage(
   dashboardHeader(title = "Multi-Touch Attribution"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Summary Dashboard", tabName = "sumdashboard", icon = icon("th")),
-      menuItem("Attribution Dashboard", tabName = "attrdashboard", icon = icon("th")),
-      menuItem("Channel Performance", tabName = "channelreport", icon = icon("dashboard")),
-      menuItem("Campaign Performance", tabName = "campaignreport", icon = icon("dashboard")),
-      menuItem("Path Report", tabName = "pathreport", icon = icon("dashboard"))
+      menuItem("Summary Dashboard", tabName = "sumdashboard", icon = icon("bar-chart")),
+      menuItem("Attribution Dashboard", tabName = "attrdashboard", icon = icon("list-alt")),
+      menuItem("Channel Performance", tabName = "channelreport", icon = icon("line-chart")),
+      menuItem("Campaign Performance", tabName = "campaignreport", icon = icon("pie-chart")),
+      menuItem("Path Report", tabName = "pathreport", icon = icon("arrows-alt"))
       
     )
   ),
@@ -23,21 +23,19 @@ dashboardPage(
       tabItem(tabName = "sumdashboard",
               fluidRow(
                 # A static infoBox
-                box(title = "Budget", 250000,width = 2, background = "maroon"),
-                box(title = "Budget Used", 50000,width = 2, background = "maroon"),
-                box(title = "Conversion",120000,width = 2, background = "maroon"),
-                box(title = "Cost per Conversions",3.6,width = 2, background = "maroon"),
-                box(title = "Revenue Generated", paste0(120000),width = 2, background = "maroon"),
-                box(title = "Revenue Generated", paste0(120000),width = 2, background = "maroon")
+                infoBox("Budget", 250000, icon = icon("dollar"),color = "purple",width = 2),
+                infoBox("Budget Used", 50000, icon = icon("hourglass-end"),color = "orange",width = 2),
+                infoBox("Conversion",120000, icon = icon("history"),color = "green", width = 2),
+                infoBox("Cost per Conversions", paste0(120000), icon = icon("credit-card"), color = "blue",width = 3),
+                infoBox("Revenue Generated", paste0(120000), icon = icon("bell"),color = "teal", width = 3)
               ),
-              
               fluidRow(
                 box(title="Histogram",status="primary",solidHeader = TRUE,collapsible = TRUE,plotOutput("plot1",height=250)),
                 
                 box(title="Inputs",status = "warning",solidHeader = TRUE,
-                  "Box content here", br(), "More box content",
-                  sliderInput("slider", "Slider input:", 1, 100, 50),
-                  textInput("text", "Text input:")
+                    "Box content here", br(), "More box content",
+                    sliderInput("slider", "Slider input:", 1, 100, 50),
+                    textInput("text", "Text input:")
                 )
               ),
               fluidRow(
@@ -51,30 +49,25 @@ dashboardPage(
               )
       ),
       tabItem(tabName = "attrdashboard",
-     
-              dateRangeInput('dateRange',
-                             label = 'Date range input: yyyy-mm-dd',
-                             start = Sys.Date() - 2, end = Sys.Date() + 2
-              ),
               
-       fluidRow(
-         box(title="Histogram",status="primary",solidHeader = TRUE,collapsible = TRUE,plotOutput("plot1",height=250)),
-         
-         box(title="Inputs",status = "warning",solidHeader = TRUE,
-             "Box content here", br(), "More box content",
-             sliderInput("slider", "Slider input:", 1, 100, 50),
-             textInput("text", "Text input:")
-         )
-       ),
-       fluidRow(
-         tabBox(
-           title = "First tabBox",
-           # The id lets us use input$tabset1 on the server to find the current tab
-           id = "tabset1", height = "250px",
-           tabPanel("Tab1", "First tab content"),
-           tabPanel("Tab2", "Tab content 2")
-         )
-       )
+              fluidRow(
+                box(title="Histogram",status="primary",solidHeader = TRUE,collapsible = TRUE,plotOutput("plot1",height=250)),
+                
+                box(title="Inputs",status = "warning",solidHeader = TRUE,
+                    "Box content here", br(), "More box content",
+                    sliderInput("slider", "Slider input:", 1, 100, 50),
+                    textInput("text", "Text input:")
+                )
+              ),
+              fluidRow(
+                tabBox(
+                  title = "First tabBox",
+                  # The id lets us use input$tabset1 on the server to find the current tab
+                  id = "tabset1", height = "250px",
+                  tabPanel("Tab1", "First tab content"),
+                  tabPanel("Tab2", "Tab content 2")
+                )
+              )
       )
       ,
       

51dfcaf601249c1aba59821e5b6d78576630efac
shanjivkumar <shanjivkumar@gmail.com>
2017-07-19 20:49:05 +0530

    Shanjiv latest

--- server.R
+++ server.R
@@ -1,7 +1,9 @@
 library(shiny)
 library(shinydashboard)
-<<<<<<< HEAD
 library(ggplot2)
+library(data.table)
+library(datasets)
+library(DT)
 buget<-read.csv("D:/satheesh/Multi touch attribution/Rshiny/Multi-touch-Attribution/Budget.csv")
 
 server <- function(input,output){
@@ -9,20 +11,11 @@ server <- function(input,output){
     ggplot(data=buget,aes(x=date,y=roi))+geom_bar(stat="identity")},height = 400,width = 400)
   output$plot3 <- renderPlot({
     hist(buget$roi)
-  })
-=======
-library(data.table)
-library(datasets)
-library(DT)
-
-server <- function(input, output) {
-  
-  
   output$mytable = DT::renderDataTable({mtcars})
-  
-  ##output$table <- renderTable(iris)
-  ##output$mytable <- renderDataTable({iris})
-  
-    
->>>>>>> ae71f06821a9b26099d0afc294b187bca129f378
+  })
 }
+
+
+
+
+

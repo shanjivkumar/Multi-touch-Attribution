d3eba09d5b19db103145d9a46c6e8a785262c9cc
shanjivkumar <shanjivkumar@gmail.com>
2017-07-19 21:45:24 +0530

    Changes to attribution

--- server.R
+++ server.R
@@ -11,7 +11,10 @@ server <- function(input,output){
     ggplot(data=buget,aes(x=date,y=roi))+geom_bar(stat="identity")},height = 400,width = 400)
   output$plot3 <- renderPlot({
     hist(buget$roi)
-  output$mytable = DT::renderDataTable({mtcars})
+  output$mytable1 = renderDataTable(buget)
+  output$mytable2 = renderDataTable(buget)
+  output$mytable3 = renderDataTable(buget)
+  
   })
 }
 

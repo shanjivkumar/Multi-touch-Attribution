a639158ecdf3fc125d2c93c0864fa648a911f9c6
shanjivkumar <shanjivkumar@gmail.com>
2017-07-19 22:24:02 +0530

    Reverted all

---
+++ server.R
@@ -0,0 +1,27 @@
+library(shiny)
+library(shinydashboard)
+library(ggplot2)
+library(data.table)
+library(datasets)
+library(DT)
+buget<-read.csv("D:/i2Decisions/17. Multi-touch attribution/2. Codes/Multi-touch-Attribution/Budget.csv")
+
+server <- function(input,output){
+  output$plot2<-renderPlot({
+    ggplot(data=buget,aes(x=date,y=roi))+geom_bar(stat="identity")})
+  output$plot3 <- renderPlot({
+    hist(buget$roi)
+    output$mytable1 = renderDataTable(buget)
+    output$mytable2 = renderDataTable(buget)
+    output$mytable3 = renderDataTable(buget)
+    
+  })
+  
+}
+
+
+
+
+
+
+
---
+++ ui.R
@@ -0,0 +1,195 @@
+#install.packages("shinythemes")
+library(shiny)
+library(shinydashboard)
+#library(shinythemes)
+library(DT)
+library(ggplot2)
+
+
+ui <- dashboardPage(skin = "purple",
+                    dashboardHeader(title = "Multi-Touch Attribution"),
+                    
+                    
+                    
+                    dashboardSidebar(
+                      
+                      sidebarMenu(
+                        menuItem("Summary Dashboard", tabName = "sumdashboard", icon = icon("bar-chart")),
+                        menuItem("Attribution Dashboard", tabName = "attrdashboard", icon = icon("list-alt")),
+                        menuItem("Channel Performance", tabName = "channelreport", icon = icon("line-chart")),
+                        menuItem("Campaign Performance", tabName = "campaignreport", icon = icon("pie-chart")),
+                        menuItem("Path Report", tabName = "pathreport", icon = icon("arrows-alt"))
+                        
+                        
+                      ),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      br(),
+                      sidebarUserPanel("Powered By : I2 Decisions", subtitle = NULL, image = NULL),
+                      sidebarUserPanel("www.i2decisions.com",image = "AAEAAQAAAAAAAAjdAAAAJDk0ZjAwNTU5LWVkZmMtNGY4Yy05MzkzLWZmNmMxYWI2YTNlYQ.png"),
+                      
+                      
+                      
+                      img(src="AAEAAQAAAAAAAAjdAAAAJDk0ZjAwNTU5LWVkZmMtNGY4Yy05MzkzLWZmNmMxYWI2YTNlYQ.png",height=40,width=40)
+                      
+                      
+                      
+                      
+                      
+                      ##img(src="AAEAAQAAAAAAAAjdAAAAJDk0ZjAwNTU5LWVkZmMtNGY4Yy05MzkzLWZmNmMxYWI2YTNlYQ.png",height=40,width=40)
+                      
+                      
+                      ##img(src="AAEAAQAAAAAAAAjdAAAAJDk0ZjAwNTU5LWVkZmMtNGY4Yy05MzkzLWZmNmMxYWI2YTNlYQ.png",height=40,width=100)
+                      
+                      
+                    ),
+                    
+                    dashboardBody(
+                      
+                      
+                      tabItems(
+                        
+                        # First tab content
+                        tabItem(tabName = "sumdashboard",
+                                fluidRow(
+                                  # A static infoBox
+                                  
+                                  box(tags$b("Budget"),br(), 250000,width = 2, background = "olive"),
+                                  box(tags$b("Budget Used"),br(), 50000,width = 2, background = "olive"),
+                                  box(tags$b("Conversion"),br(),120000,width = 2, background = "olive"),
+                                  box(tags$b("Cost per Conversions"),br(),3.6,width = 2, background = "olive"),
+                                  box(tags$b("Revenue Generated"),br(), paste0(120000),width = 2, background = "olive"),
+                                  box(tags$b("Revenue Generated"),br(), paste0(120000),width = 2, background = "olive")
+                                ),
+                                fluidRow(
+                                  box(plotOutput("plot2", height = 250)),
+                                  box(plotOutput("plot3", height = 250))
+                                  
+                                ),
+                                fluidRow(
+                                  
+                                  tabBox(
+                                    title = "First tabBox",
+                                    # The id lets us use input$tabset1 on the server to find the current tab
+                                    id = "tabset1", height = "250px",
+                                    tabPanel("Tab1", "First tab content"),
+                                    tabPanel("Tab2", "Tab content 2")
+                                  )
+                                )
+                                
+                        ),
+                        ################################################################################################################################################
+                        
+                        tabItem(tabName = "attrdashboard",
+                                
+                                #///////////////////////////////////////////////////////
+                                frow1 <- fluidRow(
+                                  tabBox(
+                                    title = "Data Viewer"
+                                    ,width = 12
+                                    ,id = "dataTabBox"
+                                    ,tabPanel(
+                                      title = "Sales by Model"
+                                      ,dataTableOutput("mytable1")
+                                    )
+                                    ,tabPanel(
+                                      title = "Sales by Quarter"
+                                      ,dataTableOutput("mytable2")
+                                    )
+                                    ,tabPanel(
+                                      title = "Prior Year Sales"
+                                      ,dataTableOutput("mytable3")
+                                    )
+                                  )
+                                )
+                                
+                                #///////////////////////////////////////////////////////////
+                                ## navbarPage("Multi-Touch Attribution",
+                                ## tabPanel("Summary Dashboard"),
+                                ## tabPanel("Attribution Dashboard"),
+                                ## tabPanel("Channel Performance")),
+                                
+                                # fluidPage(theme = "bootstrap.css"),
+                                #  fluidPage(theme = shinytheme("cyborg")),
+                                
+                                # dateRangeInput('dateRange',
+                                # label = 'Date range - "yyyy-mm-dd"',
+                                # start = Sys.Date() - 2, end = Sys.Date() + 2
+                                # ),
+                                
+                                #fluidRow(
+                                #box(title="Histogram",status="primary",solidHeader = TRUE,collapsible = TRUE,plotOutput("plot1",height=250)),
+                                #box(title="Inputs",status = "warning",solidHeader = TRUE,
+                                #"Box content here", br(), "More box content",
+                                # sliderInput("slider", "Slider input:", 1, 100, 50),
+                                # textInput("text", "Text input:")
+                                #)
+                                #),
+                                # fluidRow(
+                                #   tabBox(
+                                #     title = "First tabBox",
+                                #     # The id lets us use input$tabset1 on the server to find the current tab
+                                #     id = "tabset1", height = "250px",
+                                #     tabPanel("Tab1", "First tab content"),
+                                #     tabPanel("Tab2", "Tab content 2")
+                                #   )
+                                # ),
+                                # dataTableOutput('mytable'),
+                                
+                                # fluidPage(
+                                # h2("The mtcars data"),
+                                # dataTableOutput("mytable")
+                                
+                                #fluidPage(downloadButton("report", "Generate report"))
+                                #)
+                        ),
+                        ################################################################################################################################################
+                        
+                        #--------------------------------------------------Channel report---------------------------------------------------------
+                        #------------------------------------------------------------------------------------------------------------------------------------
+                        tabItem(tabName = "channelreport",
+                                
+                                dateRangeInput('dateRange',
+                                               label = 'Date range input: yyyy-mm-dd',
+                                               start = Sys.Date() - 2, end = Sys.Date() + 2
+                                ),
+                                
+                                fluidRow(
+                                  box(title="Histogram",status="primary",solidHeader = TRUE,collapsible = TRUE,plotOutput("plot1",height=250)),
+                                  
+                                  box(title="Inputs",status = "warning",solidHeader = TRUE,
+                                      "Box content here", br(), "More box content",
+                                      sliderInput("slider", "Slider input:", 1, 100, 50),
+                                      textInput("text", "Text input:")
+                                  )
+                                )
+                                
+                                #------------------------------------------------------------------------------------------------------------------------------------
+                                #------------------------------------------------------------------------------------------------------------------------------------
+                        )
+                      )
+                    )
+)

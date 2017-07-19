4f80526a066e966bc0b95cb4ea60dce0ff8bdb4e
Satheesh Manikandan <csesatheesh94@gmail.com>
2017-07-19 21:52:13 +0530

    Error rectified

--- server.R
+++ server.R
@@ -8,7 +8,7 @@ buget<-read.csv("D:/satheesh/Multi touch attribution/Rshiny/Multi-touch-Attribut
 
 server <- function(input,output){
   output$plot2<-renderPlot({
-    ggplot(data=buget,aes(x=date,y=roi))+geom_bar(stat="identity")},)
+    ggplot(data=buget,aes(x=date,y=roi))+geom_bar(stat="identity")})
   output$plot3 <- renderPlot({
     hist(buget$roi)
   output$mytable1 = renderDataTable(buget)
@@ -16,13 +16,12 @@ server <- function(input,output){
   output$mytable3 = renderDataTable(buget)
   
   })
-<<<<<<< HEAD
-}
-=======
+
 }
 
 
 
 
 
->>>>>>> d3eba09d5b19db103145d9a46c6e8a785262c9cc
+
+

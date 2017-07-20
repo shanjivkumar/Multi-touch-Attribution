86325ccea72c95416eb1e88ed02a28091aa1a9a3
Satheesh Manikandan <csesatheesh94@gmail.com>
2017-07-20 17:06:51 +0530

    minor summary

--- server.R
+++ server.R
@@ -16,7 +16,13 @@ server <- function(input,output){
       ggtitle("Top 5 Channels")
   })
   output$plot3 <- renderPlot({
-    hist(buget$roi)
+    ggplot(data=buget,aes(x=factor(date),y=roi,fill=channel)) +  
+      geom_bar(position = "dodge", stat="identity") + ylab("ROI On Conversions") + 
+      xlab("Date") + theme(legend.position="bottom" 
+                           ,plot.title = element_text(size=15, face="bold")) + 
+      ggtitle("Top 5 Channels")
+  })
+
     output$mytable1 = renderDataTable(buget)
     
     output$mytable2 = renderDataTable(channelpath)
@@ -26,11 +32,6 @@ server <- function(input,output){
     # output$mytable2 = renderDataTable(buget)
     # output$mytable3 = renderDataTable(iris)
     #
-  })
-  
-}
-
-
-
+  }
 
 
